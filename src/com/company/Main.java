c72c3d73fc430144d17c44e797531e612eca4c77
fstudioil <fillko@ya.ru>
2017-12-17 11:53:31 +0200

    1. Папка .idea должна быть исключена 2. строка 15 - методы с маленькой буквы 3. 18 строка - может падать на NullPointerException (NPE) 4. метод ComparePass не предполагает работу с null (null может быть возвращен в строках 16 и 17) 5. 39 строка может упасть на NPE, т.к. в классе Writer при вызове write(String str) вызывается str.length() что автоматически приведет в NPE. 6. 50 строка - не закрыт поток 7. 65 строка - блоки if else целесообразно выделять фигурными скобками для увеличения читабельности, даже когда речь идет про одну строку 8. строка 68: если вылетит ошибка из строки 66, то 68 строка просто проглотит ошибку, вернув несуществующий файл. Результат метода продолжает использоваться далее, приводя к новым ошибкам на верхних уровнях

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,67 +6,69 @@ import java.nio.file.Paths;
 public class Main {
 
     public static void main(String[] args) {
-        String pass=getPass();
-        writePassToFile(pass);
-        ComparePass();
-    }
-
-
-    private static void ComparePass() {
-        String pass1=readPassFromFile(getFile());
-        String pass2=readPassFromFile(new File( Main.class.getResource("/password.txt").getPath()));
-        if (pass1.equals(pass2))
-        {  System.out.println("Пароли совпали" +pass1);}
-        else
-            System.out.println("Пароли не совпали. В файле было "+ pass1 + ". В системе" +pass2);
-
-    }
-
-    private static String readPassFromFile(File file) {
-
-        try (BufferedReader br=new BufferedReader(new FileReader(file)) ){
-            return br.readLine();
+        String pass = null;
+        try {
+            pass = getPass();
+            writePassToFile(pass);
+            comparePass();
         } catch (IOException e) {
             e.printStackTrace();
-            return null;
         }
 
     }
 
-    private static void writePassToFile(String pass) {
+
+    private static void comparePass() throws IOException {
+        String pass1 = readPassFromFile(getFile());
+        String pass2 = readPassFromFile(new File(Main.class.getResource("/password.txt").getPath()));
+        if (pass1.equals(pass2)) {
+            System.out.println("Пароли совпали" + pass1);
+        } else {
+            System.out.println("Пароли не совпали. В файле было " + pass1 + ". В системе" + pass2);
+        }
+
+    }
+
+    private static String readPassFromFile(File file) throws IOException {
+
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            return br.readLine();
+        }
+
+    }
+
+    private static void writePassToFile (String pass) throws IOException {
 
         try (FileWriter fw = new FileWriter(getFile())) {
             fw.write(pass);
         }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
-    private static String getPass() {
-        System.out.println("Введит пароль:");
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        try {
-            return br.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-            return  null;
+    private static String getPass() throws IOException {
+
+        String pass = null;
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            while (pass == null || pass.isEmpty()) {
+                System.out.println("Введит пароль:");
+                pass = br.readLine();
+                if (pass.isEmpty()) {
+                    System.out.println("Введенный пароль пуст");
+                }
+            }
         }
+        return pass;
     }
 
 
-    private static File getFile()
-    {
+    private static File getFile()  throws IOException {
         File folder = new File(Paths.get("").toAbsolutePath().toString() + File.separator + "files");
         if (!folder.exists()) {
             folder.mkdir();
         }
         File file = new File(folder, "password.txt");
-        if (!file.exists()) try {
-            file.createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        if (!file.exists())
+        {file.createNewFile();}
         return file;
     }
 }
